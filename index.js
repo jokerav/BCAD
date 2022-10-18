abb648ec63521cb4279a77c5a4a51a02726d2932
jokerav <jokeravdeveloper@gmail.com>
2022-10-18 14:37:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const canvas = document.querySelector(".canvas");
 const btn = document.querySelector(".btn");
+
+canvas.addEventListener("mousedown", (e) => mouseClick(e));
+
+if (canvas.getContext) {
+  const ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(75, 50);
+  ctx.lineTo(100, 75);
+  ctx.lineTo(100, 25);
+  ctx.stroke();
+}
+
+const mouseClick = (e) => {
+  console.log("x", e.offsetX);
+  console.log("y", e.offsetY);
+};
